aa7c2ee00d7d313a416f5a589167b93fa979237b
null <91502871+null8R@users.noreply.github.com>
2023-06-13 23:01:13 +0900

    文字追加

--- src/js/birthday-count.js
+++ src/js/birthday-count.js
@@ -13,21 +13,31 @@ function main(){
 
   var diffDates = bTarget - now;
 
-  var days = diffDates / ( 1000 * 60 * 60 * 24 );
+  var days = Math.floor(diffDates / ( 1000 * 60 * 60 * 24 ));
   diffDates = diffDates % ( 1000 * 60 * 60 * 24 );
 
-  var hours = diffDates / ( 1000 * 60 * 60 );
+  var hours = Math.floor(diffDates / ( 1000 * 60 * 60 ));
   diffDates = diffDates % ( 1000 * 60 * 60 );
 
-  var minutes = diffDates / ( 1000 * 60 );
+  var minutes = Math.floor(diffDates / ( 1000 * 60 ));
   diffDates = diffDates % ( 1000 * 60);
 
-  var seconds = diffDates / 1000;
+  var seconds = Math.floor(diffDates / 1000);
 
-  document.getElementById('days').innerHTML = Math.floor(days);
-  document.getElementById('hours').innerHTML = Math.floor(hours);
-  document.getElementById('minutes').innerHTML = Math.floor(minutes);
-  document.getElementById('seconds').innerHTML = Math.floor(seconds);
+  if(bTarget - 31536000000 >= now){
+    document.getElementById('countdown').style.display = 'none';
+    document.getElementById('happy-birthday').style.display = 'flex';
+    document.getElementById('happy-birthday').innerHTML = "誕生日おめでとう！！";
+  }
+  else{
+    document.getElementById('countdown').style.display = '';
+    document.getElementById('happy-birthday').innerHTML = "";
+  }
+
+  document.getElementById('days').innerHTML = ("0" + days).slice(-3);
+  document.getElementById('hours').innerHTML = ("0" + hours).slice(-2);
+  document.getElementById('minutes').innerHTML = ("0" + minutes).slice(-2);
+  document.getElementById('seconds').innerHTML = ("0" + seconds).slice(-2);
 }
 
 setInterval('main()',1000);
